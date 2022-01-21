4685a52e56b0a1428de6ab1df2214e4840d3dbdd
sigalit leybman <sigalitleybman@gmail.com>
2022-01-21 09:20:44 +0200

    added info popup

--- app/src/main/java/com/example/worldwideski2/MainActivity.java
+++ app/src/main/java/com/example/worldwideski2/MainActivity.java
@@ -53,6 +53,14 @@ public class MainActivity extends AppCompatActivity
                 startActivity(intent);
             }
         });
+
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, popupInfoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     /**
