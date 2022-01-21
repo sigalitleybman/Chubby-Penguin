c5c7b22743b46676ab0e9817c18714e17aed9845
sigalit leybman <sigalitleybman@gmail.com>
2022-01-21 09:28:46 +0200

    added Level activity

---
+++ app/src/main/java/com/example/worldwideski2/LevelActivity.java
@@ -0,0 +1,13 @@
+package com.example.worldwideski2;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LevelActivity extends AppCompatActivity {
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
+}
